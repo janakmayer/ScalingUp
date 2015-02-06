8f8f3799031fc14131977321628b74f6010ea57a
Janak Mayer <janak.mayer@gmail.com>
2015-02-05 19:28:16 -0500

    Fixing type issues

--- gpfs-ngram-assignment/get_google_ngrams.py
+++ gpfs-ngram-assignment/get_google_ngrams.py
@@ -1,20 +1,20 @@
-"""Multithreaded google 2-gram downloader
+"""Multithreaded google bigram downloader
 
 This script uses workerpoool for multithreading, to enable more than one file to be downloaded at once
-to speed the process of downloading 2-gram data from http://storage.googleapis.com/books/ngrams/books/datasetsv2.html
+to speed the process of downloading bigram data from http://storage.googleapis.com/books/ngrams/books/datasetsv2.html
 """
 
 import urllib
 import sys
 import workerpool
 
-URL_TEMPLATE = 'http://storage.googleapis.com/books/ngrams/books/{}'
+URL_TEMPLATE = 'http://storage.googleapis.com/books/ngrams/books/{fname}'
 FILE_TEMPLATE = 'googlebooks-eng-all-2gram-20090715-{index}.csv.zip'
 
 
 def download(index):
     fname = FILE_TEMPLATE.format(index=index)
-    url = URL_TEMPLATE.format(fname)
+    url = URL_TEMPLATE.format(fname=fname)
     print 'Retrieving: ' + fname + '\n'
     urllib.urlretrieve(url=url, filename=fname)
     print 'Successfully downloaded: ' + fname + '\n'
@@ -25,8 +25,8 @@ if __name__ == '__main__':
     stop = int(sys.argv[2])  # second command line argument is stopping index
     index_list = range(start, stop)
 
-    # Initialize a pool with 5 threads
-    pool = workerpool.WorkerPool(size=5)
+    # Initialize a pool with 4 threads
+    pool = workerpool.WorkerPool(size=4)
 
     # Call the download function once for each index number, feeding it the index number as an argument
     pool.map(download, index_list)
