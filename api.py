e0de09c9967bab193e0c15f22c8a5409f23b86d3
Janak Mayer <janak.mayer@gmail.com>
2015-02-10 00:16:52 -0500

    API now includes mumbler component

--- api.py
+++ api.py
@@ -4,6 +4,7 @@ from flask.ext.restful.utils import cors
 from flask.ext.runner import Runner
 from api_functions import download_parse
 import os
+import json
 
 app = Flask(__name__)
 runner = Runner(app)
@@ -16,6 +17,14 @@ parser.add_argument('end', type=int)
 parser.add_argument('api_key', type=str)
 parser.add_argument('word', type=str)
 
+bi_gram = None
+
+
+def load_bigram():
+    with open('bi_gram.json', 'r') as json_file:
+        global bi_gram
+        bi_gram = json.load(json_file)
+        json_file.close()
 
 # API CLASSES
 class GetData(Resource):
@@ -24,13 +33,28 @@ class GetData(Resource):
         if args['api_key'] == os.environ['API_KEY']:
             start = args['start']
             end = args['end']
-            return download_parse.run_downloader(start, end)
+            response = download_parse.run_downloader(start, end)
+            return response
+        else:
+            return {"error": "invalid API key"}
+
+
+class BiGram(Resource):
+    def get(self):
+        args = parser.parse_args(strict=True)
+        global bi_gram
+        if bi_gram == None:
+            load_bigram()
+        if args['api_key'] == os.environ['API_KEY']:
+            word = args['word']
+            return bi_gram.get(word, {})
         else:
             return {"error": "invalid API key"}
 
 
 # API ROUTING
 api.add_resource(GetData, '/data')
+api.add_resource(BiGram, '/bigram')
 
 if __name__ == '__main__':
     runner.run()
